1ad88c7c79de03ce477633de9d3e8c353eebc6c1
Erick Jose Fernandez Arce <erickjosefernandezarce9@gmail.com>
2026-08-20 18:37:04 -0400

    Update name and surname in prueba.py

--- prueba.py
+++ prueba.py
@@ -1,6 +1,6 @@
 #este es un archivo .py esto no sera ignorado por si acaso.
 #solo ignoraremos los .txt
- nombre = "Erick"
-  apellido = "Fernandez"
+ nombre = "Erick Jose"
+  apellido = "Fernandez Arce"
 
   print("hola " + nombre + " " + apellido)
