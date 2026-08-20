e3121b0d5ea71960d97d880557d08eb2b7a9ea4b
Erick Jose Fernandez Arce <erickjosefernandezarce9@gmail.com>
2026-08-20 19:14:18 -0400

    Change name from 'Erick Jose' to 'Enrique Jose'

--- prueba.py
+++ prueba.py
@@ -1,6 +1,6 @@
 #este es un archivo .py esto no sera ignorado por si acaso.
 #solo ignoraremos los .txt
- nombre = "Erick Jose"
+ nombre = "Enrique Jose"
   apellido = "Fernandez Arce"
 
   print("hola " + nombre + " " + apellido)
