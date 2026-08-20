9542c16b116abb9d11ef31132b077bed0ef36e0b
Erick <erickjosefernandezarce9@gmail.com>
2026-08-20 19:13:09 -0400

    solucionado baby

--- prueba.py
+++ prueba.py
@@ -1,6 +1,6 @@
 #este es un archivo .py esto no sera ignorado por si acaso.
 #solo ignoraremos los .txt
- nombre = "Pepechamo"
-  apellido = "Fernandez"
+nombre = "Pepechamo"
+apellido = "Fernandez"
 
-  print("hola " + nombre + " " + apellido)
+print("hola " + nombre + " " + apellido)
